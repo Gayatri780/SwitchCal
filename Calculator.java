faa5e7de3a579bf7fee30707a3dc7a13d8200adb
Gayatri780 <gayatriapatil2003@gmail.com>
2025-04-23 13:56:32 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,49 @@
+package Pack1;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=5;
+		Scanner sc=new Scanner(System.in);
+		int cal= sc.nextInt();
+		switch(cal)
+		{
+		case 1:
+		   int c=a+b;
+			System.out.println(c);
+			   break;
+		case 2:
+	    	int d=a-b;
+		    System.out.println(d);
+		     break;
+		case 3:
+	    	int e=a/b;
+		    System.out.println(e);
+		     break;
+		case 4:
+	    	int f=a%b;
+		    System.out.println(f);
+		     break;
+		case 5:
+	    	int g=a^b;
+		    System.out.println(g);
+		     break;
+		default:
+			System.out.println("false");
+		
+		
+		
+		
+
+	}
+
+	}
+
+
+	}
+
+
